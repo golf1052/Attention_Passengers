e6a7ca8c3b2f24d56b5b21765e56ddfafb8ac039
Sanders Lauture <golf1052@live.com>
2015-07-03 13:31:11 -0400

    Started working on response message
    BUT I HAVE TO LEAVE

--- mbta2.py
+++ mbta2.py
@@ -18,15 +18,6 @@ stops_by_route_cache = []
 routes_by_stop_cache = []
 lowest_chain = None
 
-def get_directional_data(from_station, direction, time='12h'):
-    global time_format
-    if time == '24h':
-        time_format = '%H:%M'
-    else:
-        time_format = '%I:%M %p'
-
-    from_station = shorten_names(from_station)
-
 def get_from_to_data(from_station, to_station, time='12h'):
     global time_format
     global file_transfers
@@ -51,40 +42,23 @@ def get_from_to_data(from_station, to_station, time='12h'):
     to_station_lines = _lines_from_routes_data(to_station_route_data)
     _try_find_transfer_chain(from_station_id, to_station_id, from_station_lines, to_station_lines, TransferChain(), file_transfers)
     global lowest_chain
-    chain = lowest_chain
-    print 'tmp'
-    # new stops file will contain
-        # line station is on
-        # station name (id)
-        # inbound station stop id
-        # outbound station stop id
-    # 1 check to see if from and to stations are on the same line if so go to 2, if not go to 4
-    # 2 get the next train times from the from station
-    # 3 see what time that train will arrive at the to station
-    # example instruction set
-        # ruggles to downtown crossing
-        # what time can we leave ruggles
-        # what time will we get to downtown crossing
-
-    # 4 THE HARD PART?! Find the transfer chain
-        # the stops we must transfer at
-        # example oak grove to riverside
-            # transfer at north station to Green-E
-            # transfer at park street to Green-D
-        # example alewife to wonderland
-            # transfer at downtown crossing to Orange
-            # transfer at state to Blue
-    # 5 get the next train times from the from station
-    # 6 see what time that train will arrive at the transfer station
-    # 7 transfer to the new line
-    # 8 repeat 5 through 7 until we are on our to station line
-    # example instruction set
-        # also see above
-        # alewife to wonderland is actually
-        # alewife to downtown crossing
-        # downtown crossing to state
-        # state to wonderland
-
+    if lowest_chain == None:
+        return 'Could not find route from ' + from_station + ' to ' + to_station
+    total_times = [0, 0, 0]
+    final_message = ''
+    for transfer in lowest_chain.transfers:
+        final_message += transfer._from_name + transfer.to_name + '\n'
+        departures = _retrieve_schedule_by_stop(transfer._from_id)
+        message = ''
+        for mode in departures['mode']:
+            for route in mode['route']:
+                for direction in route['direction']:
+                    for trip in direction['trip']:
+                        arrivals = _retrieve_schedule_by_trip(trip['trip_id'])
+                        for stop in arrivals['stop']:
+                            if stop['stop_id'] == transfer.to_id:
+                                f_time = datetime.datetime.fromtimestamp(trip['sch_arr_dt']).strftime(time_format)
+                                t_time = datetime.datetime.fromtimestamp(stop['sch_arr_dt']).strftime(time_format)
 
 # Helper methods
 
@@ -128,14 +102,24 @@ def _try_find_transfer_chain(from_station, to_station, from_station_lines, to_st
     for to_line in to_station_lines:
         for from_line in from_station_lines:
             if from_line['route_id'] == to_line['route_id']:
-                done_transfer_chain = copy.deepcopy(transfer_chain)
-                done_transfer_chain.add_transfer(from_station, to_station, from_line['route_id'])
-                if lowest_chain == None:
-                    lowest_chain = done_transfer_chain
-                else:
-                    if len(done_transfer_chain.transfers) < len(lowest_chain.transfers):
-                        lowest_chain = done_transfer_chain
-                return done_transfer_chain
+                _from_line_stops = _retrieve_stops_by_route_cached(from_line['route_id'])
+                for direction in _from_line_stops['direction']:
+                    _station_index = 0
+                    for i in range(len(direction['stop'])):
+                        if direction['stop'][i]['parent_station'] == from_station:
+                            _station_index = i
+                            break
+                    for i in range(_station_index + 1, len(direction['stop']), 1):
+                        if direction['stop'][i]['parent_station'] == to_station:
+                            done_transfer_chain = copy.deepcopy(transfer_chain)
+                            done_transfer_chain.add_transfer(from_station, direction['stop'][_station_index]['stop_id'], direction['stop'][_station_index]['parent_station_name'], to_station, direction['stop'][i]['stop_id'], direction['stop'][i]['parent_station_name'], from_line['route_id'])
+                            if lowest_chain == None:
+                                lowest_chain = done_transfer_chain
+                            else:
+                                if len(done_transfer_chain.transfers) < len(lowest_chain.transfers):
+                                    lowest_chain = done_transfer_chain
+                            return done_transfer_chain
+                return None
             from_line_stops = _retrieve_stops_by_route_cached(from_line['route_id'])
             for direction in from_line_stops['direction']:
                 station_index = 0
@@ -154,13 +138,20 @@ def _try_find_transfer_chain(from_station, to_station, from_station_lines, to_st
                                 if new_transfer_chain.tries > 4:
                                     # we've gone too far
                                     return None
-                                new_transfer_chain.add_transfer(from_station, transfer['station'], from_line['route_id'])
+                                new_transfer_chain.add_transfer(from_station, direction['stop'][station_index]['stop_id'], direction['stop'][station_index]['parent_station_name'], transfer['station'], direction['stop'][i]['stop_id'], direction['stop'][i]['parent_station_name'], from_line['route_id'])
                                 new_from_station = transfer['station']
                                 new_from_station_route_data = _retrieve_routes_by_stop_cached(new_from_station)
                                 new_from_station_lines = _lines_from_routes_data(new_from_station_route_data)
                                 _try_find_transfer_chain(new_from_station, to_station, new_from_station_lines, to_station_lines, new_transfer_chain, file_transfers)
     return None
     # ¯\_(ツ)_/¯
+
+def _create_response_message(transfer, trip, start_time, total_time, message):
+    arrivals = _retrieve_schedule_by_trip(trip['trip_id'], start_time)
+    for stop in arrivals['stop']:
+        if stop['stop_id'] == transfer.to_id:
+            f_time = datetime.datetime.fromtimestamp(trip['sch_arr_dt']).strftime(time_format)
+            t_time = datetime.datetime.fromtimestamp(stop['sch_arr_dt']).strftime(time_format)
 
 # MBTA request helper methods
 
@@ -171,8 +162,12 @@ def _send_request(endpoint, parameters):
     r = requests.get(base_url + endpoint, params=parameters)
     return r.json()
 
-def _retrieve_schedule_by_stop(train_id, max_trips=3):
-    payload = {'max_time': 600, 'stop': train_id, 'max_trips': max_trips}
+def _retrieve_schedule_by_stop(train_id, max_trips=3, time=None):
+    payload = None
+    if time == None:
+        payload = {'max_time': 600, 'stop': train_id, 'max_trips': max_trips}
+    else:
+        payload = {'max_time': 600, 'stop': train_id, 'max_trips': max_trips, 'datetime': time}
     data = _send_request('schedulebystop', payload)
     return data
 
@@ -204,14 +199,23 @@ def _retrieve_stops_by_route(route):
     data = _send_request('stopsbyroute', payload)
     return data
 
+def _retrieve_schedule_by_trip(trip, time=None):
+    payload = None
+    if time == None:
+        payload = {'trip': trip}
+    else:
+        payload = {'trip': trip, 'datetime': time}
+    data = _send_request('schedulebytrip', payload)
+    return data
+
 # classes
 class TransferChain:
     def __init__(self):
         self.transfers = []
         self.tries = 0
 
-    def add_transfer(self, _from, to, line):
-        self.transfers.append(Transfer(_from, to, line))
+    def add_transfer(self, _from, _from_id, _from_name, to, to_id, to_name, line):
+        self.transfers.append(Transfer(_from, _from_id, _from_name, to, to_id, to_name, line))
 
     def contains_already(self, station, line):
         for transfer in self.transfers:
@@ -220,10 +224,14 @@ class TransferChain:
         return False
 
 class Transfer:
-    def __init__(self, _from, to, line):
+    def __init__(self, _from, _from_id, _from_name, to, to_id, to_name, line):
         self._from = _from
+        self._from_id = _from_id
+        self._from_name = _from_name
         self.to = to
+        self.to_id = to_id
+        self.to_name = to_name
         self.line = line
 
 if __name__ == '__main__':
-    get_from_to_data('wonderland', 'lechmere')
+    get_from_to_data('ruggles', 'mit')
